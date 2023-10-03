ed157c9131d5f3acae450221f9d1ea15af7c6b62
Pedro Ferreira <pedroferreiracjr@gmail.com>
2023-10-03 18:19:55 -0300

    conclusão desafio - sintaxe

---
+++ src/me/dio/conta_banco/ContaTerminal.java
@@ -0,0 +1,59 @@
+package me.dio.conta_banco;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+		// obtendo o número da conta do cliente através do terminal
+		Integer numero = obterNumero(scanner);
+
+		// obtendo a agência do cliente através do terminal
+		String agencia = obterAgencia(scanner);
+
+		// obtendo o nome do cliente através do terminal
+		String nomeCliente = obterNomeCliente(scanner);
+
+		// obtendo o saldo inicial da conta bancária através do terminal
+		Double saldo = obterSaldoInicial(scanner);
+		
+		Conta contaBancaria = new Conta(numero, agencia, nomeCliente, saldo);
+		imprimirInformacoesConta(contaBancaria);
+		
+		scanner.close();
+	}
+
+	private static Integer obterNumero(Scanner scanner) {
+		System.out.println("Informe o número da conta bancária: ");
+		Integer numero = scanner.nextInt();
+		return numero;
+	}
+
+	private static String obterAgencia(Scanner scanner) {
+		System.out.println("Informe a agência bancária: ");
+		String agencia = scanner.next();
+		return agencia;
+	}
+
+	private static String obterNomeCliente(Scanner scanner) {
+		System.out.println("Informe o nome do cliente da conta bancária: ");
+		scanner.next();
+		String nomeCliente = scanner.nextLine();
+		return nomeCliente;
+	}
+	
+	private static Double obterSaldoInicial(Scanner scanner) {
+		System.out.println("Informe o saldo inicial da conta bancária: ");
+		Double saldo = scanner.nextDouble();
+		return saldo;
+	}
+	
+	private static void imprimirInformacoesConta(Conta conta) {
+		System.out.println("***********************************************");
+		System.out.printf("Olá [%s], obrigado por criar uma conta em nosso banco, sua agência é [%s], conta [%d], e seu saldo [%.2f] já está disponível parar saque.%n",
+		conta.getNomeCliente(), conta.getAgencia(), conta.getNumero(), conta.getSaldo());
+		System.out.println("***********************************************");
+	}
+}
